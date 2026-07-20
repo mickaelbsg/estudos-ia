14d72e775959ecf20a68ffaa52bcf0f42543de16
Mickael Bergson Soares Gomes <105133913+mickaelbsg@users.noreply.github.com>
2026-07-19 21:02:14 -0300

    fix: correct Firebase API key and add measurement id

--- firebase-config.js
+++ firebase-config.js
@@ -5,12 +5,13 @@
  */
 window.ESTUDOS_IA_FIREBASE = {
   firebaseConfig: {
-    apiKey: "AIzaSyBwJ0l8eVDJiuYUf_s_0r9SJXvXcIVfkwY",
+    apiKey: "AIzaSyBwJ0l8eVDJiuYUf_s_Or9SJXvXcIVfkwY",
     authDomain: "estudo-ia-17f3a.firebaseapp.com",
     projectId: "estudo-ia-17f3a",
     storageBucket: "estudo-ia-17f3a.firebasestorage.app",
     messagingSenderId: "932616978524",
-    appId: "1:932616978524:web:a235b6c620de4e1562a699"
+    appId: "1:932616978524:web:a235b6c620de4e1562a699",
+    measurementId: "G-NEJL3ZG1XG"
   },
 
   // Preencha após o primeiro login. O UID aparece no Firebase Authentication.
